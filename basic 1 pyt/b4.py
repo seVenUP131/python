8afe32769b1b27e6fcc815f1c3c8443d7015d796
seVenUP_Ol <146541708+seVenUP131@users.noreply.github.com>
2024-10-27 19:51:23 +0300

    Delete basic 1 pyt directory

--- basic 1 pyt/b4.py
+++
@@ -1,3 +0,0 @@
-text = "hhhabchghhh"
-text = text.replace("h", "H", text.count('h')-1).replace('H','h',1) 
-print(text) 
--- basic 1 pyt/b6.py
+++
@@ -1,5 +0,0 @@
-numbers = [200, 123, -587]
-
-for number in numbers:
-    last_digit = abs(number) % 10  
-    print(f"Последняя цифра числа {number}: {last_digit}")
